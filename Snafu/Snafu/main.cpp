6aed1e5105148e967a40ca9fe77cd7764fe73cb0
Alexey <https://www.youtube.com/watch?v=dQw4w9WgXcQ>
2023-02-19 14:04:23 +0100

    done

--- Snafu/Snafu/main.cpp
+++ Snafu/Snafu/main.cpp
@@ -1,4 +1,13 @@
+#include <fstream>
+constexpr const char* svg = 
+    "<svg width=\"3840\" height=\"2160\" viewBox=\"0 0 3840 2160\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n"
+    "<rect x=\"200\" y=\"200\" width=\"600\" height=\"600\" stroke=\"black\"/>\n"
+    "<circle cx=\"500\" cy=\"500\" r=\"200\" stroke=\"green\"/>\n"
+    "</svg>";
+
 int main()
 {
+    std::ofstream out( "pic.svg" );
+    out << svg;
     return 0;
 }
