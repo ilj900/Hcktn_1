0755293f1e30b509b8fa003ba5812d0c88d72c68
Roman <RVasilyev@luxoft.com>
2023-02-19 17:46:33 +0100

    Timer added.

--- src/application.cpp
+++ src/application.cpp
@@ -1,5 +1,8 @@
 #include "application.h"
 
+#include <chrono>
+#include <iostream>
+
 FApplication::FApplication()
 {
 	Canvas = std::make_shared<FCanvas>(3840, 2160);
@@ -7,10 +10,15 @@ FApplication::FApplication()
 
 int FApplication::Run()
 {
-	// Draw Circle
+	auto Start = std::chrono::system_clock::now();
+
 	Canvas->DrawQuad(400, 500, 500, 0.3f, 0.3f, 0.9f);
 
 	Canvas->DrawCircle(400, 1200, 1200, 0.3f, 0.3f, 0.9f);
+
+	auto End = std::chrono::system_clock::now();
+	auto Elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(End - Start);
+	std::cout << "Time: " << Elapsed.count() << ".ms\n";
 
 	Canvas->Save("Drawing.bmp");
 
