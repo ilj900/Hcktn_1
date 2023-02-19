f130058e3af5030f0ac55a42bf715e2aa462e390
pantech48 <evgeniy.putrin@gmail.com>
2023-02-19 14:48:02 +0100

    Sam sdelal libu Otvechayu!

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "EasyBMP.hpp"
+#include <vector>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world";
-    return 0;
+  EasyBMP::Image img(3840, 2160, "sample.bmp", EasyBMP::RGBColor(255, 255, 0));
+  for (int y = 1000; y < 2000; ++y) {
+    for (int x = 1000; x < 2000; ++x) {
+        img.SetPixel(x, y, EasyBMP::RGBColor(0, 255, 0));
+    }
+  }
+  img.DrawCircle(512, 512, 512, EasyBMP::RGBColor(0, 255, 0), true);
+  img.Write();
+  return 0;
 }
